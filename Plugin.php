4cf9dce326cff539d1d877ee8be7957d63b9c3d9
Asim Husanovic <asim.husanovic@gmail.com>
2016-11-16 14:55:31 +0300

    Remove unnecessary code

--- Plugin.php
+++ Plugin.php
@@ -4,8 +4,6 @@ namespace Kanboard\Plugin\TargetNew;
 
 use Kanboard\Core\Plugin\Base;
 use Kanboard\Core\Translator;
-use Kanboard\Core\Security\AuthenticationManager;
-use Kanboard\Event\AuthSuccessEvent;
 
 class Plugin extends Base
 {
@@ -13,8 +11,6 @@ class Plugin extends Base
 
     public function initialize()
     {
-        $this->dispatcher->addListener(AuthenticationManager::EVENT_SUCCESS, array($this, 'onLoginSuccess'));
-
         $this->helper->register('TargetNewHelper', '\Kanboard\Plugin\TargetNew\Helper\TargetNewHelper');
         $this->template->hook->attach('template:user:sidebar:actions', 'TargetNew:user/sidebar/actions');
         $this->template->hook->attach('template:layout:head', 'TargetNew:layout/head');
@@ -23,11 +19,6 @@ class Plugin extends Base
     public function onStartup()
     {
         Translator::load($this->languageModel->getCurrentLanguage(), __DIR__.'/Locale');
-    }
-
-    public function onLoginSuccess(AuthSuccessEvent $event) {
-        $user_id = $this->userSession->getId();
-        $user = $this->userModel->getById($user_id);
     }
 
     public function getClasses()
